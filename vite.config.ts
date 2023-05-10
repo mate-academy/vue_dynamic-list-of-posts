f09e81268ea8f7e1912e1a5957be3786a799f5c3
Oleg Medushevskiy <oleg.medushevskiy@sigma.software>
2023-05-10 21:27:46 +0300

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue_dynamic-list-of-posts",
   plugins: [react()],
   resolve: {
     // alias: {
@@ -13,5 +14,4 @@ export default defineConfig({
     //   shared: path.resolve(__dirname, "./src/shared"),
     // },
   },
-  base: "/vue_dynamic-list-of-posts/",
 });
