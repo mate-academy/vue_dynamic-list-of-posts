7b3af74f7ebcf0ae8278a5498d7735d651ecc0de
Oleg Medushevskiy <oleg.medushevskiy@sigma.software>
2023-05-10 21:32:10 +0300

    Fixed homepage link

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/vue_dynamic-list-of-posts",
   plugins: [react()],
   resolve: {
-    // alias: {
-    //   components: path.resolve(__dirname, "./src/components"),
-    //   api: path.resolve(__dirname, "./src/api"),
-    //   utils: path.resolve(__dirname, "./src/utils"),
-    //   routes: path.resolve(__dirname, "./src/routes"),
-    //   shared: path.resolve(__dirname, "./src/shared"),
-    // },
+    alias: {
+      components: path.resolve(__dirname, "./src/components"),
+      api: path.resolve(__dirname, "./src/api"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      routes: path.resolve(__dirname, "./src/routes"),
+      shared: path.resolve(__dirname, "./src/shared"),
+    },
   },
 });
