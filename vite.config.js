03d90b70d278eae985b5870781191460b3096f3d
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-07-17 13:53:52 +0300

    finish deploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: process.env.NODE_ENV === 'production' ? '/vue_dynamic-list-of-posts/' : '/',
   // resolve: {
   //   alias: {
   //     '@': fileURLToPath(new URL('./src', import.meta.url))
