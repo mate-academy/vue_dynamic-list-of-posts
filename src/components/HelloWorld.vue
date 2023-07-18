570ca82eac0e2c88e5e193feb05440f05f715100
Vadym Dubniak <dubnyakv@gmail.com>
2023-07-18 22:38:38 +0300

    test 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,8 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      You’ve successfully created a project with Vue
+      
     </h3>
   </div>
 </template>
