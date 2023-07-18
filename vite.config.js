df11ac379cc7b545031a6783693b947fcace120a
Vadym Dubniak <dubnyakv@gmail.com>
2023-07-18 22:03:34 +0300

    initial

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:'/vue_dynamic-list-of-posts/',
+  
+  plugins: [
+    vue(),
+  ],
+})
