ec1a33a9fbec601a49f8428cd3357808920f7f74
artkas03 <zhonayer2017@gmail.com>
2023-10-06 18:10:19 +0300

    changed vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue_dynamic-list-of-posts/" : "/",
-};
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  publicPath: '/vue-dynamic-list-of-posts/',
+  transpil
+});
