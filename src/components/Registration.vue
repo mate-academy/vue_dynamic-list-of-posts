0c75b428d8f45f65c8a0e3246134fe7beeda2bbe
denys2 <brownexpert@gmail.com>
2024-03-04 23:10:59 +0200

    solution 23^10

---
+++ src/components/Registration.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    emits: ['changeNameHandler'],
+  })
+</script>
+
+<template>
+  <div class="field">
+    <label class="label" for="user-name"> Your Name </label>
+
+    <div class="control has-icons-left">
+      <input
+        type="text"
+        id="user-name"
+        name="name"
+        class="input"
+        placeholder="Enter your name"
+        required
+        @input="$emit('changeNameHandler', $event)"
+      />
+
+      <span class="icon is-small is-left">
+      <i class="fas fa-user" />
+    </span>
+    </div>
+  </div>
+</template>
