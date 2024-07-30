6801f93d05d031b1502f1d8a787f698384a958bb
Olaf <olaf.chuszno@gmail.com>
2024-07-30 14:03:52 +0200

    Create vue project skeleton

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import TheWelcome from "./components/TheWelcome.vue";
+
+export default {
+  data() {
+    return {
+      email: String,
+      userId: Boolean,
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <!-- <HelloWorld msg="You did it!" /> -->
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
