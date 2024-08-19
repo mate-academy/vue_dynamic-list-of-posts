7d7ca1740206328a3b28a05b9f6be52b0a0f283a
Faiza Hamid <hamid.faiza811@gmail.com>
2024-08-19 19:11:20 +0300

    add base markup

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <main class="section">
+    <div class="container">
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <div class="tile is-child box is-success">
+            <div class="block">
+              <UserSelector
+                users="{users}"
+                selectedUser="{selectedUser}"
+                onChangeSelectedUser="{handleSelectedUser}"
+              />
+            </div>
+
+            <div class="block" data-cy="MainContent">
+              {!selectedUser && (
+              <p data-cy="NoSelectedUser">No user selected</p>
+              )} {isLoadingPosts && <Loader />} {loadPostError &&
+              <Errors dataCy="{loadPostError}" />} {!usersPosts.length &&
+              isPostsShown && (
+              <div class="notification is-warning" data-cy="NoPostsYet">
+                No posts yet
+              </div>
+              )} {usersPosts.length > 0 && isPostsShown && (
+              <PostsList
+                posts="{usersPosts}"
+                selectedPost="{selectedPost}"
+                onSelectedPost="{setSelectedPost}"
+              />
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div data-cy="Sidebar">
+          <div class="tile is-child box is-success ">
+            {selectedPost && <PostDetails post="{selectedPost}" />}
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/styles/App.scss';
+import './assets/styles/Loader.scss';
+
+
+import 'bulma/bulma.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+
+createApp(App).mount('#app')
