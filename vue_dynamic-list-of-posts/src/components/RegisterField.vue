70b10e2a37807c3fd87deafd69a50ad0d21bfb72
Tetiana Veremchuk <tetianaverem@gmail.com>
2024-08-28 10:47:24 +0300

    solution

---
+++ vue_dynamic-list-of-posts/src/components/RegisterField.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  emits: ['changeName'],
+  methods: {
+    onChangeName($event) {
+      this.$emit('changeName', $event.target.value);
+    },
+  },
+};
+</script>
+
+<template>
+<div class="field">
+  <label class="label" htmlFor="user-name"> Your Name </label>
+
+  <div class="control has-icons-left">
+    <input
+      type="text"
+      id="user-name"
+      name="name"
+      class="input"
+      placeholder="Enter your name"
+      @input="onChangeName"
+      required
+      minlength="4"
+    />
+
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+  </div>
+
+  <!-- <p class="help is-danger">error message</p> -->
+</div>
+
+</template>
---
+++ vue_dynamic-list-of-posts/src/main.js
@@ -0,0 +1,8 @@
+import 'bulma';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
