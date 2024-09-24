41a60925886d2612165f95ada29d3aa121b0ac1e
MarekSax <mareksax@wp.pl>
2024-09-24 11:34:03 +0200

    chore: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  base: '/vue_dynamic-list-of-posts/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
