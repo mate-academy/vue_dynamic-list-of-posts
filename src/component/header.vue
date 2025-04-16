fdc076e29d471282758e9ff3ff75597b783a76d9
marichkamt <emilycry1999@gmail.com>
2025-04-16 16:47:21 +0200

    solution

--- src/component/header.vue
+++ src/component/header.vue
@@ -1,3 +1,14 @@
+<script>
+export default {
+  name: 'Header',
+  props: {
+    user: {},
+  },
+  emits: ['logout']
+}
+</script>
+
+<template>
 <nav className="navbar" role="navigation" aria-label="main navigation">
   <div className="navbar-item">
     <h2 className="is-size-4">Vue List Of Posts</h2>
@@ -6,11 +17,12 @@
     <div className="navbar-item">
       <div className="buttons">
         <div className="mr-5 mb-2">
-          <p>User: {user.name}</p>
+          <p>User: {{ user.name }}</p>
         </div>
 
-        <a className="button is-light"> Logout </a>
+        <a className="button is-light" @click="$emit('logout')">Logout</a>
       </div>
     </div>
   </div>
 </nav>
+</template>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
