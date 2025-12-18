6e858a263d7b951ba17034c64ffc64036b715fdc
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-12-18 09:22:54 -0300

    code after code review

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <nav>
-      <router-link to="/home"></router-link>
-      <router-link to="/login"></router-link>
+      <router-link to="/home">Home</router-link>
+      <router-link to="/login">Login</router-link>
     </nav>
 
     <main>
