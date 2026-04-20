98d794854d54194937e00892c31cae2148e91a13
Mykyta Kalniev <nikitamal4ik@gmail.com>
2026-04-20 11:20:48 +0200

    Set up deploy

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PostsList from './components/PostsList.vue'
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <PostsList />
+</template>
+
+<style scoped></style>
